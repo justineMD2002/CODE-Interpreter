add78a26b009685ebbe0e62376ce290a395a8b63
lancevincentsalera <lancevincent.salera@cit.edu>
2024-03-24 15:47:32 +0800

    parser - variable declarations

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -5,9 +6,25 @@ public class App {
         Scanner scan = new Scanner(System.in);
         while (true) {
             System.out.println("Enter code:");
-            String code = scan.nextLine();
-            Lexer lexer = new Lexer(code);
-            lexer.lex();
+            StringBuilder builder = new StringBuilder();
+            if(builder.toString().equals("exit")) break;
+            while (true) {
+                String code = scan.nextLine();
+
+                builder.append(code).append('\n');
+                if (code.equalsIgnoreCase("END CODE")) {
+                    break; // Exit the loop if 'exit' is entered
+                }
+            }
+            Lexer lexer = new Lexer(builder.toString());
+            List<Token> tokens = lexer.lex();
+            Parser parser = new Parser(tokens);
+            if(parser.parse()) {
+                System.out.println("Parsing complete");
+            } else {
+                System.out.println("There was an error parsing your code");
+            }
         }
+        scan.close();
     }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -10,22 +10,108 @@ class Parser {
     }
 
     // call parse method to start parsing
-    public void parse() {
-        program();
+    public boolean parse() {
+        return program();
     }
 
-    //matches the type of the token
+    // matches the type of the token
     private boolean match(Token.Type expectedType) {
         if (currentTokenIndex < tokens.size() && tokens.get(currentTokenIndex).getType() == expectedType) {
+
+            System.out.println("Index: " + currentTokenIndex);
             currentTokenIndex++;
+            return true;
+        }
+        // System.out.println(currentTokenIndex + " not matched");
+        return false;
+    }
+
+    // Program --> BEGIN CODE VariableDeclarations ExecutableCode END CODE
+    private boolean program() {
+        if(match(Token.Type.BeginContainer)) {
+            if(match(Token.Type.EndContainer)) {
+                return true;
+            } else {
+                System.out.println(currentTokenIndex);
+                if(variableDeclarations()) {
+                    System.out.println("true");
+                    return match(Token.Type.EndContainer);
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+
+    // VariableDeclarations -> VariableDeclaration VariableDeclarations | ε
+    private boolean variableDeclarations() {
+        if (variableDeclaration()) {
+            variableDeclarations();
             return true;
         }
         return false;
     }
 
-    // Program --> BEGIN CODE VariableDeclarations ExecutableCode END CODE
-    public boolean program() {
-        return match(Token.Type.BeginContainer) && (match(Token.Type.Int) || match(Token.Type.Char)
-                || match(Token.Type.Float) || match(Token.Type.Bool)) && match(Token.Type.EndContainer);
+    // VariableDeclaration -> DataType VariableList
+    private boolean variableDeclaration() {
+        if (DataType()) {
+            if (variableList()) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    // VariableList -> VariableName VariableList'
+    private boolean variableList() {
+        if (variableName()) {
+            if (variableListPrime()) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    // VariableList' -> ',' VariableName VariableList' | ε
+    private boolean variableListPrime() {
+        if (match(Token.Type.Comma)) {
+            if (variableName()) {
+                return variableListPrime();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    // DataType -> INT | CHAR | BOOL | FLOAT
+    private boolean DataType() {
+        return match(Token.Type.Int) || match(Token.Type.Char) || match(Token.Type.Bool) || match(Token.Type.Float);
+    }
+
+    // VariableName -> Identifier
+    private boolean variableName() {
+        if (match(Token.Type.Identifier)) {
+            return assignment();
+        }
+        System.out.println("False");
+        return false;
+    }
+
+    // Assignment -> '=' Expression
+    private boolean assignment() {
+        if (match(Token.Type.Assign)) {
+            if (value()) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    // Expression -> Identifier | Num
+    private boolean value() {
+        return match(Token.Type.Identifier) || match(Token.Type.Num) || match(Token.Type.NumFloat);
     }
 }
